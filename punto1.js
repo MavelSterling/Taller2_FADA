45893bc4b98b0d607466ae3e20b4fcdcff5d0439
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-12-27 00:31:03 -0500

    Avance punto 1

---
+++ punto1.js
@@ -0,0 +1,85 @@
+const buscarOficinaDisponible = (arreglo = [], tiempo) => {
+
+    for (let i = 0; i < arreglo.length; i++) {
+        if (tiempo == 0) {
+            console.log("cerooo", arreglo)
+            return arreglo
+        } else if (arreglo[i] == tiempo) {
+            console.log("arreglo[i] == tiempo", arreglo)
+            arreglo[i] = 0;
+            console.log(arreglo)
+            return arreglo
+        } else if (arreglo[i] > tiempo) {
+            console.log("arreglo[i] sdfsdfsd antes ", arreglo)
+
+            console.log("arreglo", arreglo)
+            console.log("arreglo[i]", arreglo[i])
+            console.log("tiempo", tiempo)
+            console.log("arreglo[i] > tiempo", arreglo[i] > tiempo)
+            console.log("i", i)
+
+            arreglo[i] = (arreglo[i] - tiempo);
+            console.log("arreglo[i] sdfsdfsd desp", arreglo)
+
+            return arreglo
+        }
+    }
+
+    return arreglo;
+}
+
+const estaOficinaDisponible = (arreglo = [], tiempo) => {
+
+    for (let i = 0; i < arreglo.length; i++) {
+        if (tiempo == 0) {
+            return true;
+        } else if (arreglo[i] == tiempo) {
+            return true
+        } else if (arreglo[i] > tiempo) {
+            return true
+        }
+    }
+
+    return false;
+}
+
+const colaDeAtencion = (N, T, A = []) => {
+    let oficinasMinimas = 0;
+    let oficinasDisponibles = [];
+
+    for (let i = 0; i < N; i++) {
+        if ((A[i] + A[i + 1]) < T && (A[i] + A[i + 1] != 0) && estaOficinaDisponible(oficinasDisponibles, A[i] + A[i + 1])) {
+            oficinasDisponibles = buscarOficinaDisponible(oficinasDisponibles, (A[i] + A[i + 1]));
+            console.log("A[i]", A[i])
+            console.log("T - (A[i] + A[i + 1])", T - (A[i] + A[i + 1]))
+            oficinasDisponibles[i] = (T - (A[i] + A[i + 1]));
+            console.log("oficinasDisponiblesssss: ", oficinasDisponibles);
+            i++;
+            oficinasMinimas++;
+        } /* else if ((A[i] + A[i + 1]) == T) {
+            oficinasDisponibles.push(0);
+            i++;
+            oficinasMinimas++;
+        } */ else if (A[i] <= T) {
+            console.log("entreee", A[i])
+            let arregloActualizado = estaOficinaDisponible(oficinasDisponibles, A[i]) ? buscarOficinaDisponible(oficinasDisponibles, A[i]) : false;
+
+            if (!arregloActualizado) {
+                oficinasDisponibles.push(T - A[i]);
+                oficinasMinimas++;
+            }
+
+            console.log("arregloActualizado: ", arregloActualizado);
+
+        }
+
+    }
+    console.log("oficinasDisponibles: ", oficinasDisponibles);
+    return oficinasMinimas;
+
+}
+
+const tiempoMaxAtencion = 8;
+const tiempoAtencion = [0, 4, 0, 6, 4]; // [1,2,4]
+const numeroOficinas = colaDeAtencion(tiempoAtencion.length, tiempoMaxAtencion, tiempoAtencion);
+console.log("numeroOficinas: ", numeroOficinas);
--- punto2.js
+++ punto2.js
@@ -1,44 +1,37 @@
 
+const countingSort = (arr, min, max) => {
+    const count = {};
+    // First populate the count object
+    for (let i = min; i <= max; i++) {
+        count[i] = 0;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        count[arr[i]] += 1;
+    }
 
-const merge = (left, right) => {
-
-    let arr = [];
-
-    while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            arr.push(left.shift());
-        } else {
-            arr.push(right.shift());
+    // Then, iterate over count's properties from min to max:
+    const sortedArr = [];
+    for (let i = min; i <= max; i++) {
+        while (count[i] > 0) {
+            sortedArr.push(i);
+            count[i]--;
         }
     }
-    return [...arr, ...left, ...right]
+    return sortedArr;
+};
 
-}
-
-const mergeSort = (array) => {
-
-    let half = Math.floor((array.length) / 2);
-    // Base case or terminating case
-    if (array.length < 2) {
-        return array
-    }
-
-    const left = array.splice(0, half)
-    return merge(mergeSort(left), mergeSort(array))
-}
-
-const alquiler = (m, n, k, M = [], N = []) => {
+const arrendarApto = (m, n, k, M = [], N = []) => {
 
     let personasQuePuedenArrendar = 0;
 
-    const ordenadoM = mergeSort(M);
-    const ordenadoN = mergeSort(N);
+    const ordenadoM = countingSort(M, 1, 100000);
+    const ordenadoN = countingSort(N, 1, 100000);
 
     let iterarM = 0;
     let iterarN = 0;
 
-    /*     console.log("ordenadoM", ordenadoM)
-        console.log("ordenadoN", ordenadoN) */
+        console.log("ordenadoM", ordenadoM)
+        console.log("ordenadoN", ordenadoN)
 
     while (iterarM < m || iterarN < n) {
         //Controlamos que el arreglo no se desborde
@@ -66,6 +59,5 @@ let tolerancia = 5;
 let capacidadPersonas = [30, 60, 75];
 let costoApartamentos = [60, 45, 80, 60];
 
-let personasQuePuedenArrendar = alquiler(costoApartamentos.length, capacidadPersonas.length, tolerancia, capacidadPersonas, costoApartamentos);
+let personasQuePuedenArrendar = arrendarApto(costoApartamentos.length, capacidadPersonas.length, tolerancia, capacidadPersonas, costoApartamentos);
 console.log("personasQuePuedenArrendar son: ", personasQuePuedenArrendar)
-  
