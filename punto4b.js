8aa3c91b4a4a409d0399dfbb1031603ebd09a846
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2023-01-16 00:22:10 -0500

    avanzar en punto4

--- punto4b.js
+++ punto4b.js
@@ -3,6 +3,7 @@ class Arbol {
     constructor() {
         this.arbolPalabras = {};
         this.esElFinalDeLaPalabra = false
+        this.arregloFinal = [];
     }
     // Insertar palabra en el arbol
     insertar(palabra) {
@@ -15,51 +16,55 @@ class Arbol {
             arbolActual = arbolActual[letra];
         }
         arbolActual.esElFinalDeLaPalabra = true;
+        arbolActual.palabraCompleta = palabra;
     }
-    // encontrar letra
-    encontrarLetra(palabra) {
+    // encontrar prefijo
+    encontrarPrefijo(palabra) {
+        this.arregloFinal = [];
         let arbolActual = this.arbolPalabras;
-        console.log("arbol.arbolPalabras: ", JSON.stringify(this.arbolPalabras))
-
-        let prefix = "";
+        let prefijo = "";
         for (let i = 0; i < palabra.length; i++) {
             let letra = palabra[i];
-            if (arbolActual.esElFinalDeLaPalabra) {
-                return prefix;
-            }
             if (arbolActual[letra]) {
-                prefix += letra;
+                prefijo += letra;
                 arbolActual = arbolActual[letra];
             } else {
-
-                return null;
+                break;
             }
         }
-        return arbolActual.esElFinalDeLaPalabra ? prefix : null;
+        prefijo !== "" ? this.autocompletarPalabra(arbolActual, prefijo) : this.arregloFinal.push("")
+        return this.arregloFinal;
+    }
+
+    // recorrer arbol para autocompletar palabra
+    autocompletarPalabra(arbolActual, prefijo) {
+        if (arbolActual.esElFinalDeLaPalabra) {
+            this.arregloFinal.push(arbolActual.palabraCompleta);
+        }
+        for (let letra in arbolActual) {
+            if (letra !== "esElFinalDeLaPalabra" && letra !== "palabraCompleta") {
+                this.autocompletarPalabra(arbolActual[letra], prefijo + letra);
+            }
+        }
     }
 }
-
 
 
 const autocompletado = (N, OPS = []) => {
 
     const arbol = new Arbol();
-
+    let a = []
     for (let i = 0; i < N; i++) {
         if (OPS[i][0] == 1) {
             arbol.insertar(OPS[i][1])
+        } else if (OPS[i][0] == 2) {
+            a.push(arbol.encontrarPrefijo(OPS[i][1]).toString())
         }
     }
-
-    // console.log("arbol.arbolPalabras: ", JSON.stringify(arbol.arbolPalabras))
-
-    for (let i = 0; i < N; i++) {
-        if (OPS[i][0] == 2) {
-            console.log(arbol.encontrarLetra(OPS[i][1]))
-        }
-    }
+    return a
 
 }
 
-let OPS = [[1, "hola"], [1, "cabeza"], [1, "arroz"], [2, "aro"], [2, "pez"]]
-const palabraAutocompletada = autocompletado(OPS.length, OPS)
+let OPS = [[1, "hola"], [1, "cabeza"], [1, "arroz"], [2, "aro"], [2, "ar"]]
+const palabrasAutocompletadas = autocompletado(OPS.length, OPS)
+console.log("palabrasAutocompletadas: ", palabrasAutocompletadas)
